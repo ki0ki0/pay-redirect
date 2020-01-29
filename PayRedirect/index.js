c8ab6d5d48d8ee070facacdb9bace7b792ed0670
Ihor Kostiuk <igorkost@gmail.com>
2020-01-30 01:37:46 +0200

    pay using sandbox

--- PayRedirect/index.js
+++ PayRedirect/index.js
@@ -1,16 +1,33 @@
+var LiqPay = require('liqpayjs-sdk');
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
+       
+    if (req.query.account && req.query.amount) {
+        var liqpay = new LiqPay("sandbox_i76493038487", "sandbox_tvA0pMWRoa3Nx17CDqIBMPdVF5eriJ1Vc0IfhCxy");
+        const params = {
+            'action': 'pay',
+            'amount': req.query.amount,
+            'currency': 'UAH',
+            'description': 'Оплата внескiв на утримання будинку, о/р ' + req.query.account,
+            'order_id': 'order_id_1',
+            'version': '3'
+        };
+        var signature = liqpay.cnb_signature(params);
+        
+        var data = new Buffer(JSON.stringify(params)).toString('base64');
 
-    if (req.query.name || (req.body && req.body.name)) {
         context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
+            status: 303, /* Defaults to 200 */
+            headers: {
+                location: "https://www.liqpay.ua/api/3/checkout?data="+data+"&signature="+signature
+            }
         };
     }
     else {
         context.res = {
             status: 400,
-            body: "Please pass a name on the query string or in the request body"
+            body: "Query string is missing"
         };
     }
 };
