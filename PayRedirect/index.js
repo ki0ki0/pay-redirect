e6c84f55f669823d0acb5db000513c3a3d280d46
Ihor Kostiuk <igorkost@gmail.com>
2020-01-31 00:11:09 +0200

    use period

--- PayRedirect/index.js
+++ PayRedirect/index.js
@@ -1,15 +1,17 @@
 var LiqPay = require('liqpayjs-sdk');
+const util = require('util');
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
        
-    if (req.query.account && req.query.amount) {
+    if (req.query.account && req.query.amount && req.query.period) {
         var liqpay = new LiqPay(process.env["liqpay_public_key"], process.env["liqpay_private_key"]);
+        var description = util.format(process.env["liqpay_description"], req.query.period, req.query.account)
         const params = {
             'action': 'paydonate',
-            'amount': req.query.amount,
+            'amount': parseFloat(req.query.amount),
             'currency': 'UAH',
-            'description': process.env["liqpay_description"] + ' ' + req.query.account,
+            'description': description,
             'order_id': 'order_id_1',
             'version': '3',
             'result_url': process.env["liqpay_result_url"]
